57f6810e548542b851b30107a90bbedea89c9a69
“Gilary <221197@unamba.edu.pe>
2026-06-10 19:23:01 -0500

    first commit

---
+++ curso c++/ejemplo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numero = 10;
+    if (numero < 0){
+        cout<<numero<<" es negative"<<endl;
+    } else{
+        cout<<"El numero es positive";
+    }
+    return 0;
+}
+
+
